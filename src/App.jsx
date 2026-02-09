e89b2ee71d14ead073f21ce7759f877322041b15
Artem Kutsan <artemkutsan@gmail.com>
2026-02-09 09:55:53 +0100

    Initial commit: Базовая структура

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <>
+      {/* <div className="layout">
+        <Header />
+        <section id="hero"></section>
+      </div> */}
+      <main>
+        <div className="container">
+          <h1>React project Cooljob</h1>
+        </div>
+        {/* <section id=""></section> */}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
