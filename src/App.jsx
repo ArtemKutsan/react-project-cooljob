bfd8446c7e94b09758e21056d7a02febaf69af82
Artem Kutsan <artemkutsan@gmail.com>
2026-02-09 10:42:26 +0100

    update: Подключение Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // src/App.jsx
 import './App.css';
+import Header from './components/sections/Header';
 
 function App() {
   return (
     <>
-      {/* <div className="layout">
+      <div className="layout">
         <Header />
-        <section id="hero"></section>
-      </div> */}
+        {/* <section id="hero"></section> */}
+      </div>
       <main>
         <div className="container">
           <h1>React project Cooljob</h1>
