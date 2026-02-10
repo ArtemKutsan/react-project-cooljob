f074be4079d2678c3b4fb0953cd6e226245ebf30
Artem Kutsan <artemkutsan@gmail.com>
2026-02-10 14:37:15 +0100

    update: Подключены секции Hero и Categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 // src/App.jsx
 import './App.css';
 import Header from './components/sections/Header';
+import Hero from './components/sections/Hero';
 import Partners from './components/sections/Partners';
+import Categories from './components/sections/Categories';
+// import Vacancies from './components/sections/Vacancies';
 
 function App() {
   return (
     <>
       <div className="layout">
         <Header />
-        {/* <section id="hero"></section> */}
-      </div>
-      <main>
-        <div className="container">
-          <h1>React project Cooljob</h1>
-        </div>
+        <section id="hero">
+          <Hero />
+        </section>
         <section id="partners">
           <Partners />
         </section>
+      </div>
+      <main>
+        {/* <div className="container">
+          <h1>React project Cooljob</h1>
+        </div> */}
+
+        <section id="categories">
+          <Categories />
+        </section>
+
+        {/* <section id="vacancies">
+          <Vacancies />
+        </section> */}
       </main>
       {/* <Footer /> */}
     </>
